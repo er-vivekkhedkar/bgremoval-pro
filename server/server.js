3f4c4c70a8c519d79c42a0c0e613217b19f78afa
Vivek <vivekkhedkar110@gmail.com>
2025-01-21 18:17:53 +0530

    dbacc

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 
 
@@ -13,7 +14,9 @@ await connectDB()
 app.use(express.json())
 app.use(cors())
 
+
 // API Routes 
 app.get('/',(req,res)=> res.send('API Working'))
+app.use('/api/user', userRouter)
 
 app.listen(PORT, ()=> console.log(`Server is running on port `+PORT))
